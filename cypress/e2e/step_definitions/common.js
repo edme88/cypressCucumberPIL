c6145631619f48e33986b5119ae6a1b9fe71166e
EXB04891 <agustina.aliciardi@darwoft.com>
2023-10-02 15:34:14 -0300

    Se agrgeo tets visual de yvytu

--- cypress/e2e/step_definitions/common.js
+++ cypress/e2e/step_definitions/common.js
@@ -1,7 +1,11 @@
-import { Given } from "@badeball/cypress-cucumber-preprocessor";
+import { Given, Then } from "@badeball/cypress-cucumber-preprocessor";
 
 Given(`que un usuario está en la página de {string}`, (urlName) => {
   const url = Cypress.env(`${urlName.toUpperCase()}_URL`);
   cy.viewport("macbook-16");
   cy.visit(url);
+});
+
+Then(`se compara {string} con la imagen base`, (imageName) => {
+  cy.compareSnapshot(imageName);
 });
