e955a08082f94d7cf4e4a668e1df85402c618f03
EXB04891 <agustina.aliciardi@darwoft.com>
2023-10-05 15:55:51 -0300

    Se agrego descarga del formatter

--- cucumber-html-report.js
+++ cucumber-html-report.js
@@ -1,3 +1,26 @@
+//BAJAR EL FORMATER
+const https = require("https");
+const fs = require("fs");
+const path = "cucumber-json-formatter.exe";
+
+if (!fs.existsSync(path)) {
+  const file = fs.createWriteStream(path);
+  https.get(
+    "https://github.com/cucumber/json-formatter/releases/download/v19.0.0/cucumber-json-formatter-windows-amd64",
+    function (response) {
+      response.pipe(file);
+
+      file.on("Finalizo", () => {
+        file.close();
+        console.log("Descarga completa");
+      });
+    }
+  );
+} else {
+  console.log(`El archivo ${path} ya existe`);
+}
+
+//GENERAR EL REPORTE
 const report = require("multiple-cucumber-html-reporter");
 
 report.generate({
